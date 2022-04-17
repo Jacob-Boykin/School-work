e05f7a9f5f77a59ecc5a98b344d142528ff5a1bf
Jacob-Boykin <92758576+Jacob-Boykin@users.noreply.github.com>
2022-04-16 21:34:43 -0500

    Nothing beneficial
    Trying different things

--- CS201/Project5/p5.cpp
+++ CS201/Project5/p5.cpp
@@ -5,10 +5,44 @@
 #include <vector>
 
 using namespace std;
+// void printUncomp()
+// {
+
+// }
+// void printComp()
+// {
+
+// }
+// void printLetters()
+// {
+
+// }
+// int count()
+// {
+
+// }
+
 
 int main(int argc, char *argv[]) {
-
-   /* Type your code here. */
-
+   string filename(argv[2]);
+   fstream file(filename);
+   vector<string> words;
+   string temp, line, reset = "";
+   int size;
+   
+   while(getline(file, line))
+   {
+      temp = reset;
+      istringstream iss(line);
+      while(getline(iss, temp, '\t'))
+      {
+         words.push_back(temp);
+      }
+   }
+   size = words.size();
+   for(int i = 0; i < size; i++)
+   {
+      cout << words.at(i) << endl;
+   }
    return 0;
 }
